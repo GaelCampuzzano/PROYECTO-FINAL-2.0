e33532a85582106d6c4e13324b9b38c7771d6df7
Maximo <luismaximonava@gmail.com>
2024-10-14 08:29:52 -0700

    Clase Mesa

--- Mesa.cs
+++ Mesa.cs
@@ -8,5 +8,26 @@ namespace PROYECTO_FINAL_2._0
 {
     internal class Mesa
     {
+        public int Numero { get; set; }
+        public int Sillas { get; set; }
+        public bool Ocupada { get; set; }
+
+        public Mesa(int numero, int sillas)
+        {
+            Numero = numero;
+            Sillas = sillas;
+            Ocupada = false;
+        }
+
+        public void OcuparMesa()
+        {
+            Ocupada = true;
+        }
+
+        public void LiberarMesa()
+        {
+            Ocupada = false;
+        }
+
     }
 }
