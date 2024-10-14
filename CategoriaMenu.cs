5e696fff52b038988928bda75a90d00fa0f538e7
Maximo <luismaximonava@gmail.com>
2024-10-14 09:04:51 -0700

    Clase CategoriaMenu

--- CategoriaMenu.cs
+++ CategoriaMenu.cs
@@ -8,5 +8,21 @@ namespace PROYECTO_FINAL_2._0
 {
     internal class CategoriaMenu
     {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public List<Platillo> Platillos { get; set; }
+
+        public CategoriaMenu(string nombre, string descripcion)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Platillos = new List<Platillo>();
+        }
+
+        public void AgregarPlatillo(Platillo platillo)
+        {
+            Platillos.Add(platillo);
+        }
+
     }
 }
