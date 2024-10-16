4a42da1ddbfcf3519486ab9ed5a2fc5a4762df1d
Campuzano <43223041@lcb.unidep.edu.mx>
2024-10-16 07:47:27 -0700

    Restaurante

--- Comanda.cs
+++ Comanda.cs
@@ -1,5 +1,4 @@
-﻿using PROYECTO_FINAL_2._0;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
-﻿using PROYECTO_FINAL_2._0;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Restaurante2._0
 {
